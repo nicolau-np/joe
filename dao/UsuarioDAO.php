9a40966854fb3a800104ff0445624b412479870f
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 20:52:56 +0100

    criando metodo store

--- dao/UsuarioDAO.php
+++ dao/UsuarioDAO.php
@@ -11,8 +11,30 @@ class UsuarioDao extends Conexao{
 
     }
 
-    public function store(){
+    public function store(Usuario $usuario){
+        $this->resposta = null;
+        $con = $this->ligar();
+        $this->consulta = "insert into usuario (nome, sexo, "
+                . "telefone, estado, email, palavra_passe)"
+                . " values(:nome, :sexo, :telefone, :estado, :email, :palavra_passe)";
+        try {
+            $this->comando = $con->prepare($this->consulta);
+            $this->comando->bindValue(":nome", $usuario->getNome(), PDO::PARAM_INT);
+            $this->comando->bindValue(":sexo", $usuario->getSexo(), PDO::PARAM_INT);
+            $this->comando->bindValue(":telefone", $usuario->getTelefone(), PDO::PARAM_INT);
+            $this->comando->bindValue(":estado", $usuario->getEstado(), PDO::PARAM_STR);
+            $this->comando->bindValue(":email", $usuario->getEmail(), PDO::PARAM_STR);
+            $this->comando->bindValue(":palavra_passe", $usuario->getPalavra_passe(), PDO::PARAM_STR);
+            $this->comando->execute();
+            if ($this->comando):
+                $this->resposta = $con->lastInsertId();
+            endif;
+            $this->desligar();
+        } catch (PDOException $ex) {
+            echo '' . $ex->getMessage();
+        }
 
+        return $this->resposta;
     }
 
     public function show($id){
