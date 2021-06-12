e9ee46ac655111b6a63c60ead53b35a146331630
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 20:36:08 +0100

    criando os getters do modelo usuario

--- modelo/Usuario.php
+++ modelo/Usuario.php
@@ -1,1 +1,44 @@
 <?php
+
+class Usuario{
+    
+private $id;
+private $nome;
+private $sexo;
+private $email;
+private $palavra_passe;
+private $telefone;
+private $estado;
+
+
+function getId(){
+    return $this->id;
+}
+
+function getNome(){
+    return $this->nome;
+}
+
+function getSexo(){
+    return $this->sexo;
+}
+
+function getEmail(){
+    return $this->email;
+}
+
+function getPalavra_passe(){
+    return $this->palavra_passe;
+}
+
+function getTelefone(){
+    return $this->telefone;
+}
+
+function getEstado(){
+    return $this->estado;
+}
+
+
+
+}
