eefe8ca37c416fc8d7316e99779e9de6294eac48
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 21:24:53 +0100

    mudando em cadastro

--- cadastro.php
+++ cadastro.php
@@ -2,6 +2,9 @@
 include_once('./controlo/Conexao.php');
 include_once('./modelo/Usuario.php');
 include_once('./dao/UsuarioDAO.php');
+
+$usuario = new Usuario();
+$usuarioDAO = new UsuarioDAO();
 ?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
