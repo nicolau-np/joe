56768d82527338111ac613abd7e8932c6ec6722c
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 19:58:01 +0100

    mudando os links de html para php em Magisterio Primario

--- Magisterio Primario.php
+++ Magisterio Primario.php
@@ -1,209 +1,225 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Magisterioio Primario</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
-<!-- templatemo 358 carousel -->
-<!-- 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Magisterioio Primario</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+    <!-- templatemo 358 carousel -->
+    <!-- 
 Carousel Template 
 http://www.templatemo.com/preview/templatemo_358_carousel 
 -->
-<script type="text/javascript" src="js/jquery-1-4-2.min.js"></script> 
-<!--script type="text/javascript" src="/jqueryui/js/jquery-ui-1.7.2.custom.min.js"></script--> 
-<script type="text/javascript" src="js/jquery-ui.min.js"></script> 
-<script type="text/javascript" src="js/showhide.js"></script> 
-<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
-<script type="text/JavaScript" src="js/jquery.mousewheel.js"></script> 
-<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+    <script type="text/javascript" src="js/jquery-1-4-2.min.js"></script>
+    <!--script type="text/javascript" src="/jqueryui/js/jquery-ui-1.7.2.custom.min.js"></script-->
+    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/showhide.js"></script>
+    <link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" />
+    <script type="text/JavaScript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/JavaScript" src="js/slimbox2.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+    <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
 
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/ddsmoothmenu.js">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/ddsmoothmenu.js">
+    /***********************************************
+     * Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+     * This notice MUST stay intact for legal use
+     * Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+     ***********************************************/
+    </script>
 
-/***********************************************
-* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-* This notice MUST stay intact for legal use
-* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
-***********************************************/
-
-</script>
-
-<script type="text/javascript">
-
-ddsmoothmenu.init({
-	mainmenuid: "templatemo_menu", //menu DIV id
-	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
-	classname: 'ddsmoothmenu', //class added to menu's outer DIV
-	//customtheme: ["#1c5a80", "#18374a"],
-	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
-})
-
-</script> 
+    <script type="text/javascript">
+    ddsmoothmenu.init({
+        mainmenuid: "templatemo_menu", //menu DIV id
+        orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+        classname: 'ddsmoothmenu', //class added to menu's outer DIV
+        //customtheme: ["#1c5a80", "#18374a"],
+        contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+    })
+    </script>
 
 </head>
 
 <body id="subpage">
 
-<div id="templatemo_header_wrapper">
-	<div id="site_title"><h1><a href="#">Carousel Template</a></h1></div>
-     <div id="templatemo_menu" class="ddsmoothmenu">
-        <ul>
-            <li><a href="index.html" >Home</a></li>
-            <li><a href="#"class="selected">Cursos</a>
-                <ul>
-                    <li><span class="top"></span><span class="bottom"></span></li>
-                    <li><a href="Pre-escolar.html">Pré-escolar</a></li>
-                    <li><a href="Magisterio Primario.html">Magisterio Primario</a></li>
-                   
-              	</ul>
-            </li>
-            <li><a href="#">Fotos</a>
-                <ul>
-                    <li><span class="top"></span><span class="bottom"></span></li>
-                    <li><a href="Inauguração.html">Inauguração</a></li>
-                    <li><a href="Actividades.html">Actividades</a></li>
-               
-              	</ul>
-            </li>
-            <li><a href="Estrutura.html">Estrutura</a></li>
-            <li><a href="contact.html">Cadastro</a></li>
-        </ul>
-        <br style="clear: left" />
-    </div> <!-- end of templatemo_menu -->
-    <div class="cleaner"></div>
-</div>	<!-- END of templatemo_header_wrapper -->
-
-
-    <div class="cleaner"></div>
-</div> <!-- END of templatemo_main -->
-
-<div id="templatemo_main">
-	<font color="black"><p><b>Magistério Primário</b> é um curso pertencente a instituição de ensino EFP denominado escola do Magistério Primário “Júlia Lopes” que tem como objectivo formar professores qualificados capaz de ser um excelente educador do ensino de crianças compreendida de 6há 12 anos de idade de 1ª ate 6ª de modo a fornecer uma educação de qualidade com ma base sólida fornecendo oportunidade educativa a todos.</p>
-	<p>Ao entrar a este magnífica curso o aluno deve ter competências suficientes para ingressar-se neste curso dominando as seguintes disciplinas:</p>
-<ul>		
-		<li>Língua portuguesa </li>
-	<li>Matemática </li>
-	<li>Educação Musical</li>
-</ul>
-Após ter entrado a este curso o aluno deve lesionar as seguintes disciplinas:
-<p></p>
-<br>
-	10ª Classe Disciplina Lesionadas
-</br>
-<ul>
-	<li>Matemática </li>
-	<li>Língua portuguesa </li>
-	<li>Geografia</li>
-	<li>História</li>
-	<li>Química</li>
-	<li>Biologia</li>
-	<li>Empreendedorismo</li>
-	<li>Expressão Plástica</li>
-	<li>Inglês</li>
-	<li>Expressão Musical</li>
-	<li>Física</li>
-	<li>Expressão Motora</li>
-	<li>Psicologia</li>
-	<li>Nhaneca</li>
-</ul>
-11ª Classe Disciplina Lesionadas
-<ul>
-	<li>Língua portuguesa </li>
-	<li>Matemática</li>
-	<li>TDEC “Teoria do desenvolvimento de Educação Curricular”</li>
-	<li>Metodologia de Língua Portuguesa</li>
-	<li>Metodologia geografia e Historia </li>
-	<li>NEE “ Necessidade especiais educativas</li>
-	<li>PSEP” Pratica seminário estágio pedagógico”</li>
-	<li>Inglês</li>
-	<li>Expressão Plástica</li>
-	<li>Expressão Musical</li>
-	<li>Expressão Motora</li>
-	<li>Nhaneca</li>
-	</ul>
-12ª Disciplinais lesionadas 
-<ul>
-	<li>Língua portuguesa </li>
-	<li>Matemática</li>
-	<li>Metodologia de Língua Portuguesa</li>
-	<li>Metodologia geografia e Historia </li>
-	<li>PSEP“ Pratica seminário estágio pedagógico</li>
-	<li>Metodologia de estudo do meio </li>
-	<li>Inglês</li>
-	<li>Filosofia</li>
-	<li>Expressão Plástica</li>
-	<li>Expressão Musical</li>
-	<li>Expressão Motora</li>
-	<li>Nhaneca</li>
-	<li>FPSD Formação pessoal social e Deontológicas</li>
-</ul>
-
-<p>Durante o percurso de formação na 13ªclase o aluno estará preparado e capacitado fazer um casamento perfeito entre a teoria e pratica podendo fazer o “Estagio” e terá simplesmente uma disciplina denominada Higiene e Saúde, O aluno será responsável pelo se plano de aulas durante o percurso do estagio.
-
-Após ter dado por encerrar o curso na 13ª classe o aluno este capacitado para ostentar o nome de docente podendo dar aulas no ensino Primário podendo cria uma instituição de ensino Primário “Colégio ou uma cresce.
-
-</p></font>
-</div> <!-- END of templatemo_main -->
-
-<div id="templatemo_bottom_wrapper">
-    <div id="templatemo_bottom">
-    	<div class="col one_third">
-        	<h4><span></span>Menu</h4>
-            <div class="bottom_box">
-                <ul class="footer_list">
-                    <li><a href="Estrutura.html">Estrutura</a></li>
-                    <li><a href="#">Cadastoro</a></li>
-					<ul>
-					<li>Cursos</li>
-                    <li><a href="Pre-escolar.html">Pré-escolar</a></li>
-                    <li><a href="Magisterio Primario.html">Magisterio Primario</a></li>
-					</ul>
-                   </ul>  
-			</div>
+    <div id="templatemo_header_wrapper">
+        <div id="site_title">
+            <h1><a href="#">Carousel Template</a></h1>
         </div>
-        <div class="col one_third">
-        	<h4><span></span>	Contactos</h4>
-			<li>email:luciacalulu@yahoo.com.br</li>
-			<li>silcasilva7@yahoo.com.br</li>
-			<li>telefone:+244 924-410-501</li>
-			<li>+244 923-699-207</li>
-            <div class="bottom_box">
-                <ul class="twitter_post">
-                    <li></a></li>
-                    <li></li>
-                </ul>
-			</div>
-        </div>
-        <div class="col one_third no_margin_right">
-        	<center><h4><span></span>Sobre a Pagina</h4></center>
-            <div class="bottom_box">
-               <p><em>esta pagina web foi criada com objetivo de promover a publicitação das informaçães  da escola Julia lopes a nivel da midia
-			   de modo a facilitar e minimizar os gastos, esforços  tornando mais facíl o processo de aderencia a escola. </em></p><br/>
-                <div class="footer_social_button">
-                    <a href="#"><img src="images/facebook.png" title="facebook" alt="facebook" /></a>
-                    <a href="#"><img src="images/flickr.png" title="flickr" alt="flickr" /></a>
-                    <a href="#"><img src="images/twitter.png" title="twitter" alt="twitter" /></a>
-                    <a href="#"><img src="images/youtube.png" title="youtube" alt="youtube" /></a>
-                    <a href="#"><img src="images/feed.png" title="rss" alt="rss" /></a>
+        <div id="templatemo_menu" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#" class="selected">Cursos</a>
+                    <ul>
+                        <li><span class="top"></span><span class="bottom"></span></li>
+                        <li><a href="Pre-escolar.php">Pré-escolar</a></li>
+                        <li><a href="Magisterio Primario.php">Magisterio Primario</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="#">Fotos</a>
+                    <ul>
+                        <li><span class="top"></span><span class="bottom"></span></li>
+                        <li><a href="Inauguração.php">Inauguração</a></li>
+                        <li><a href="Actividades.php">Actividades</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="Estrutura.php">Estrutura</a></li>
+                <li><a href="contact.php">Cadastro</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of templatemo_menu -->
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header_wrapper -->
+
+
+    <div class="cleaner"></div>
+    </div> <!-- END of templatemo_main -->
+
+    <div id="templatemo_main">
+        <font color="black">
+            <p><b>Magistério Primário</b> é um curso pertencente a instituição de ensino EFP denominado escola do
+                Magistério Primário “Júlia Lopes” que tem como objectivo formar professores qualificados capaz de ser um
+                excelente educador do ensino de crianças compreendida de 6há 12 anos de idade de 1ª ate 6ª de modo a
+                fornecer uma educação de qualidade com ma base sólida fornecendo oportunidade educativa a todos.</p>
+            <p>Ao entrar a este magnífica curso o aluno deve ter competências suficientes para ingressar-se neste curso
+                dominando as seguintes disciplinas:</p>
+            <ul>
+                <li>Língua portuguesa </li>
+                <li>Matemática </li>
+                <li>Educação Musical</li>
+            </ul>
+            Após ter entrado a este curso o aluno deve lesionar as seguintes disciplinas:
+            <p></p>
+            <br>
+            10ª Classe Disciplina Lesionadas
+            </br>
+            <ul>
+                <li>Matemática </li>
+                <li>Língua portuguesa </li>
+                <li>Geografia</li>
+                <li>História</li>
+                <li>Química</li>
+                <li>Biologia</li>
+                <li>Empreendedorismo</li>
+                <li>Expressão Plástica</li>
+                <li>Inglês</li>
+                <li>Expressão Musical</li>
+                <li>Física</li>
+                <li>Expressão Motora</li>
+                <li>Psicologia</li>
+                <li>Nhaneca</li>
+            </ul>
+            11ª Classe Disciplina Lesionadas
+            <ul>
+                <li>Língua portuguesa </li>
+                <li>Matemática</li>
+                <li>TDEC “Teoria do desenvolvimento de Educação Curricular”</li>
+                <li>Metodologia de Língua Portuguesa</li>
+                <li>Metodologia geografia e Historia </li>
+                <li>NEE “ Necessidade especiais educativas</li>
+                <li>PSEP” Pratica seminário estágio pedagógico”</li>
+                <li>Inglês</li>
+                <li>Expressão Plástica</li>
+                <li>Expressão Musical</li>
+                <li>Expressão Motora</li>
+                <li>Nhaneca</li>
+            </ul>
+            12ª Disciplinais lesionadas
+            <ul>
+                <li>Língua portuguesa </li>
+                <li>Matemática</li>
+                <li>Metodologia de Língua Portuguesa</li>
+                <li>Metodologia geografia e Historia </li>
+                <li>PSEP“ Pratica seminário estágio pedagógico</li>
+                <li>Metodologia de estudo do meio </li>
+                <li>Inglês</li>
+                <li>Filosofia</li>
+                <li>Expressão Plástica</li>
+                <li>Expressão Musical</li>
+                <li>Expressão Motora</li>
+                <li>Nhaneca</li>
+                <li>FPSD Formação pessoal social e Deontológicas</li>
+            </ul>
+
+            <p>Durante o percurso de formação na 13ªclase o aluno estará preparado e capacitado fazer um casamento
+                perfeito entre a teoria e pratica podendo fazer o “Estagio” e terá simplesmente uma disciplina
+                denominada Higiene e Saúde, O aluno será responsável pelo se plano de aulas durante o percurso do
+                estagio.
+
+                Após ter dado por encerrar o curso na 13ª classe o aluno este capacitado para ostentar o nome de docente
+                podendo dar aulas no ensino Primário podendo cria uma instituição de ensino Primário “Colégio ou uma
+                cresce.
+
+            </p>
+        </font>
+    </div> <!-- END of templatemo_main -->
+
+    <div id="templatemo_bottom_wrapper">
+        <div id="templatemo_bottom">
+            <div class="col one_third">
+                <h4><span></span>Menu</h4>
+                <div class="bottom_box">
+                    <ul class="footer_list">
+                        <li><a href="Estrutura.php">Estrutura</a></li>
+                        <li><a href="#">Cadastoro</a></li>
+                        <ul>
+                            <li>Cursos</li>
+                            <li><a href="Pre-escolar.php">Pré-escolar</a></li>
+                            <li><a href="Magisterio Primario.php">Magisterio Primario</a></li>
+                        </ul>
+                    </ul>
                 </div>
+            </div>
+            <div class="col one_third">
+                <h4><span></span> Contactos</h4>
+                <li>email:luciacalulu@yahoo.com.br</li>
+                <li>silcasilva7@yahoo.com.br</li>
+                <li>telefone:+244 924-410-501</li>
+                <li>+244 923-699-207</li>
+                <div class="bottom_box">
+                    <ul class="twitter_post">
+                        <li></a></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col one_third no_margin_right">
+                <center>
+                    <h4><span></span>Sobre a Pagina</h4>
+                </center>
+                <div class="bottom_box">
+                    <p><em>esta pagina web foi criada com objetivo de promover a publicitação das informaçães da escola
+                            Julia lopes a nivel da midia
+                            de modo a facilitar e minimizar os gastos, esforços tornando mais facíl o processo de
+                            aderencia a escola. </em></p><br />
+                    <div class="footer_social_button">
+                        <a href="#"><img src="images/facebook.png" title="facebook" alt="facebook" /></a>
+                        <a href="#"><img src="images/flickr.png" title="flickr" alt="flickr" /></a>
+                        <a href="#"><img src="images/twitter.png" title="twitter" alt="twitter" /></a>
+                        <a href="#"><img src="images/youtube.png" title="youtube" alt="youtube" /></a>
+                        <a href="#"><img src="images/feed.png" title="rss" alt="rss" /></a>
+                    </div>
                 </div>
             </div>
         </div>
-        
-    	<div class="cleaner"></div>
-    </div> <!-- END of tempatemo_bottom -->
-</div> <!-- END of tempatemo_bottom_wrapper -->
 
-<div id="templatemo_footer_wrapper">
-    <div id="templatemo_footer"
-   <center><h3>Ecola Magisterio Primario Júlia Lopes</h3></center>
-    </div> <!-- END of templatemo_footer_wrapper -->
-</div> <!-- END of templatemo_footer -->
+        <div class="cleaner"></div>
+    </div> <!-- END of tempatemo_bottom -->
+    </div> <!-- END of tempatemo_bottom_wrapper -->
+
+    <div id="templatemo_footer_wrapper">
+        <div id="templatemo_footer" <center>
+            <h3>Ecola Magisterio Primario Júlia Lopes</h3>
+            </center>
+        </div> <!-- END of templatemo_footer_wrapper -->
+    </div> <!-- END of templatemo_footer -->
 
 </body>
+
 </html>
-           
