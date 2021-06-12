93c78ebcbd01f6aafcc304e69409ceb44be438c2
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 20:43:43 +0100

    criando o dao de usuarios

---
+++ dao/UsuarioDAO.php
@@ -0,0 +1,33 @@
+<?php  
+
+class UsuarioDao extends Conexao{
+    
+
+    public function index(){
+        
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function show($id){
+        
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update($id){
+        
+    }
+
+    public function destroy($id){
+        
+    }
+}
