4efe6b355f0b89ddde3da23c1f68545f14895bc4
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-12 19:56:02 +0100

    mudando os links de html para php em actividades

--- Actividades.php
+++ Actividades.php
@@ -1,225 +1,235 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Actividades</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/foundation.min.css">
+    <title>Actividades</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/foundation.min.css">
 
-<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
-
- 
-
-
-<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
 
 
 
-<link rel="stylesheet" href="css/stylesheet.css">
 
-<link rel="stylesheet" href="js/colorbox/colorbox.css"><!--[if lt IE 9]>
+    <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+
+
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+    <link rel="stylesheet" href="js/colorbox/colorbox.css">
+    <!--[if lt IE 9]>
 <link rel="stylesheet" href="css/ie-fixes.css">
 <!galeria-->
 
-<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
-<!-- templatemo 358 carousel -->
-<!-- 
+    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+    <!-- templatemo 358 carousel -->
+    <!-- 
 Carousel Template 
 http://www.templatemo.com/preview/templatemo_358_carousel 
 -->
-<script type="text/javascript" src="js/jquery-1-4-2.min.js"></script> 
-<!--script type="text/javascript" src="/jqueryui/js/jquery-ui-1.7.2.custom.min.js"></script--> 
-<script type="text/javascript" src="js/jquery-ui.min.js"></script> 
-<script type="text/javascript" src="js/showhide.js"></script> 
-<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
-<script type="text/JavaScript" src="js/jquery.mousewheel.js"></script> 
-<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+    <script type="text/javascript" src="js/jquery-1-4-2.min.js"></script>
+    <!--script type="text/javascript" src="/jqueryui/js/jquery-ui-1.7.2.custom.min.js"></script-->
+    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/showhide.js"></script>
+    <link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" />
+    <script type="text/JavaScript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/JavaScript" src="js/slimbox2.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+    <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
 
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/ddsmoothmenu.js">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/ddsmoothmenu.js">
+    /***********************************************
+     * Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+     * This notice MUST stay intact for legal use
+     * Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+     ***********************************************/
+    </script>
 
-/***********************************************
-* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-* This notice MUST stay intact for legal use
-* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
-***********************************************/
-
-</script>
-
-<script type="text/javascript">
-
-ddsmoothmenu.init({
-	mainmenuid: "templatemo_menu", //menu DIV id
-	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
-	classname: 'ddsmoothmenu', //class added to menu's outer DIV
-	//customtheme: ["#1c5a80", "#18374a"],
-	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
-})
-
-</script> 
+    <script type="text/javascript">
+    ddsmoothmenu.init({
+        mainmenuid: "templatemo_menu", //menu DIV id
+        orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+        classname: 'ddsmoothmenu', //class added to menu's outer DIV
+        //customtheme: ["#1c5a80", "#18374a"],
+        contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+    })
+    </script>
 
 </head>
+
 <body>
-<div id="templatemo_header_wrapper">
-	<div id="site_title"><h1><a href="#">Carousel Template</a></h1></div>
-     <div id="templatemo_menu" class="ddsmoothmenu">
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="#">Cursos</a>
-                <ul>
-                    <li><span class="top"></span><span class="bottom"></span></li>
-                    <li><a href="Pre-escolar.html">Pre-escolar</a></li>
-                    <li><a href="Magisterio Primario.html">Magisterio Primario</a></li>
-                   
-              	</ul>
-            </li>
-            <li><a href="#" class="selected">Fotos</a>
-                <ul>
-                    <li><span class="top"></span><span class="bottom"></span></li>
-                    <li><a href="Inauguração.html">Inauguração</a></li>
-                    <li><a href="Actividades.html">Actividades</a></li>
-               
-              	</ul>
-            </li>
-            <li><a href="Estrutura.html">Estrutura</a></li>
-            <li><a href="contact.html">Cadastro</a></li>
-        </ul>
-        <br style="clear: left" />
-    </div> <!-- end of templatemo_menu -->
-    <div class="cleaner"></div>
-</div>	<!-- END of templatemo_header_wrapper -->
+    <div id="templatemo_header_wrapper">
+        <div id="site_title">
+            <h1><a href="#">Carousel Template</a></h1>
+        </div>
+        <div id="templatemo_menu" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Cursos</a>
+                    <ul>
+                        <li><span class="top"></span><span class="bottom"></span></li>
+                        <li><a href="Pre-escolar.php">Pre-escolar</a></li>
+                        <li><a href="Magisterio Primario.php">Magisterio Primario</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="#" class="selected">Fotos</a>
+                    <ul>
+                        <li><span class="top"></span><span class="bottom"></span></li>
+                        <li><a href="Inauguração.php">Inauguração</a></li>
+                        <li><a href="Actividades.php">Actividades</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="Estrutura.php">Estrutura</a></li>
+                <li><a href="contact.php">Cadastro</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of templatemo_menu -->
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header_wrapper -->
 
 
     <div class="cleaner"></div>
-</div> <!-- END of templatemo_main -->
+    </div> <!-- END of templatemo_main -->
 
 
-          
 
 
-        <div class="thumbnail-block">
-          <ul class="large-block-grid-4">
+
+    <div class="thumbnail-block">
+        <ul class="large-block-grid-4">
             <li><a class="thumbnail" href="images/D.jpg"><img src="images/D.jpg" alt=""></a></li>
             <li><a class="thumbnail" href="images/golden-wheat-field.jpg"><img src="images/xs.png" alt=""></a></li>
-            <li><a class="thumbnail" href="images/cracked-earth-texture.jpg"><img src="images/cracked-earth-texture.jpg" alt=""></a></li>
-            <li><a class="thumbnail" href="images/beautiful-young-girl.jpg"><img src="images/beautiful-young-girl.jpg" alt=""></a></li>
-          </ul>
-        </div>
-        <div class="thumbnail-block">
-          <ul class="large-block-grid-4">
+            <li><a class="thumbnail" href="images/cracked-earth-texture.jpg"><img src="images/cracked-earth-texture.jpg"
+                        alt=""></a></li>
+            <li><a class="thumbnail" href="images/beautiful-young-girl.jpg"><img src="images/beautiful-young-girl.jpg"
+                        alt=""></a></li>
+        </ul>
+    </div>
+    <div class="thumbnail-block">
+        <ul class="large-block-grid-4">
             <li><a class="thumbnail" href="images/o.jpg"><img src="images/o.jpg" alt=""></a></li>
             <li><a class="thumbnail" href="images/0.jpg"><img src="images/0.jpg" alt=""></a></li>
             <li><a class="thumbnail" href="images/a.jpg"><img src="images/a.jpg" alt=""></a></li>
             <li><a class="thumbnail" href="images/c.jpg"><img src="images/c.jpg" alt=""></a></li>
-          </ul>
-        </div>
-      <footer>
-	  
-            <div class="bottom_box">
-               
-                <div class="footer_social_button">
-                </div>
-            </div>
-        </div>
-        
-    	<div class="cleaner"></div>
-    </div> <!-- END of tempatemo_bottom -->
-</div> 
-<div id="templatemo_bottom_wrapper">
-    <div id="templatemo_bottom">
-    	<div class="col one_third">
-        	<h4><span></span>Menu</h4>
-            <div class="bottom_box">
-                <ul class="footer_list">
-                    <li><a href="Estrutura.html">Estrutura</a></li>
-                    <li><a href="#">Cadastoro</a></li>
-					<li><a href="#">Cursos</a></li>
-					<ul>
-                    <li><a href="Pre-escolar.html">Pré-escolar</a></li>
-                    <li><a href="Magisterio Primario.html">Magisterio Primario</a></li>
-					</ul>
-                   </ul>  
-			</div>
-        </div>
-        <div class="col one_third">
-        	<h4><span></span>	Contactos</h4>
-			<li>email:luciacalulu@yahoo.com.br</li>
-			<li>silcasilva7@yahoo.com.br</li>
-			<li>telefone:+244 924-410-501</li>
-			<li>+244 923-699-207</li>
-            <div class="bottom_box">
-                <ul class="twitter_post">
-                    <li></a></li>
-                    <li></li>
-                </ul>
-			</div>
-        </div>
-        <div class="col one_third no_margin_right">
-        	<center><h4><span></span>Sobre a Pagina</h4></center>
-            <div class="bottom_box">
-               <p><em>esta pagina web foi criada com objetivo de promover a publicitação das informaçães  da escola Julia lopes a nivel da midia
-			   de modo a facilitar e minimizar os gastos, esforços  tornando mais facíl o processo de aderencia a escola. </em></p><br/>
-                <div class="footer_social_button">
-                    <a href="#"><img src="images/facebook.png" title="facebook" alt="facebook" /></a>
-                    <a href="#"><img src="images/flickr.png" title="flickr" alt="flickr" /></a>
-                    <a href="#"><img src="images/twitter.png" title="twitter" alt="twitter" /></a>
-                    <a href="#"><img src="images/youtube.png" title="youtube" alt="youtube" /></a>
-                    <a href="#"><img src="images/feed.png" title="rss" alt="rss" /></a>
-                </div>
-                </div>
-            </div>
-        </div>
-        
-    	<div class="cleaner"></div>
-    </div> <!-- END of tempatemo_bottom -->
-</div> <!-- END of tempatemo_bottom_wrapper -->
+        </ul>
+    </div>
+    <footer>
 
-<div id="templatemo_footer_wrapper">
-    <div id="templatemo_footer"
-   <center><h3>Ecola Magisterio Primario Júlia Lopes</h3></center>
-    </div> <!-- END of templatemo_footer_wrapper -->
-</div> <!-- END of templatemo_footer -->
- <!-- END of templatemo_footer_wrapper -->
-</div> <!-- END of templatemo_footer -->
-</footer>
-<script src="js/jquery.min1.js"></script>
-<script src="js/hoverIntent.js"></script>
-<script src="js/superfish.js"></script>
-<script src="js/colorbox/jquery.colorbox-min.js"></script>
-<script>
-Modernizr.load({
-    test: Modernizr.placeholder,
-    nope: 'js/placeholder.min.js'
-});
-function goToNewPage() {
-    if (document.getElementById('target').value) {
-        window.location.href = document.getElementById('target').value;
+        <div class="bottom_box">
+
+            <div class="footer_social_button">
+            </div>
+        </div>
+        </div>
+
+        <div class="cleaner"></div>
+        </div> <!-- END of tempatemo_bottom -->
+        </div>
+        <div id="templatemo_bottom_wrapper">
+            <div id="templatemo_bottom">
+                <div class="col one_third">
+                    <h4><span></span>Menu</h4>
+                    <div class="bottom_box">
+                        <ul class="footer_list">
+                            <li><a href="Estrutura.php">Estrutura</a></li>
+                            <li><a href="#">Cadastoro</a></li>
+                            <li><a href="#">Cursos</a></li>
+                            <ul>
+                                <li><a href="Pre-escolar.php">Pré-escolar</a></li>
+                                <li><a href="Magisterio Primario.php">Magisterio Primario</a></li>
+                            </ul>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col one_third">
+                    <h4><span></span> Contactos</h4>
+                    <li>email:luciacalulu@yahoo.com.br</li>
+                    <li>silcasilva7@yahoo.com.br</li>
+                    <li>telefone:+244 924-410-501</li>
+                    <li>+244 923-699-207</li>
+                    <div class="bottom_box">
+                        <ul class="twitter_post">
+                            <li></a></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col one_third no_margin_right">
+                    <center>
+                        <h4><span></span>Sobre a Pagina</h4>
+                    </center>
+                    <div class="bottom_box">
+                        <p><em>esta pagina web foi criada com objetivo de promover a publicitação das informaçães da
+                                escola Julia lopes a nivel da midia
+                                de modo a facilitar e minimizar os gastos, esforços tornando mais facíl o processo de
+                                aderencia a escola. </em></p><br />
+                        <div class="footer_social_button">
+                            <a href="#"><img src="images/facebook.png" title="facebook" alt="facebook" /></a>
+                            <a href="#"><img src="images/flickr.png" title="flickr" alt="flickr" /></a>
+                            <a href="#"><img src="images/twitter.png" title="twitter" alt="twitter" /></a>
+                            <a href="#"><img src="images/youtube.png" title="youtube" alt="youtube" /></a>
+                            <a href="#"><img src="images/feed.png" title="rss" alt="rss" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="cleaner"></div>
+        </div> <!-- END of tempatemo_bottom -->
+        </div> <!-- END of tempatemo_bottom_wrapper -->
+
+        <div id="templatemo_footer_wrapper">
+            <div id="templatemo_footer" <center>
+                <h3>Ecola Magisterio Primario Júlia Lopes</h3>
+                </center>
+            </div> <!-- END of templatemo_footer_wrapper -->
+        </div> <!-- END of templatemo_footer -->
+        <!-- END of templatemo_footer_wrapper -->
+        </div> <!-- END of templatemo_footer -->
+    </footer>
+    <script src="js/jquery.min1.js"></script>
+    <script src="js/hoverIntent.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/colorbox/jquery.colorbox-min.js"></script>
+    <script>
+    Modernizr.load({
+        test: Modernizr.placeholder,
+        nope: 'js/placeholder.min.js'
+    });
+
+    function goToNewPage() {
+        if (document.getElementById('target').value) {
+            window.location.href = document.getElementById('target').value;
+        }
     }
-}
-</script>
-<script>
-$(document).ready(function () {
-    $('ul.sf-menu').superfish({
-        animation: {
-            height: 'show'
-        },
-        delay: 400
+    </script>
+    <script>
+    $(document).ready(function() {
+        $('ul.sf-menu').superfish({
+            animation: {
+                height: 'show'
+            },
+            delay: 400
+        });
+        $("img.scrollToTop").click(function() {
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow");
+        });
+        $(".thumbnail").colorbox({
+            rel: "thumbnail",
+            transition: "fade",
+            width: "50%",
+            height: "50%"
+        });
     });
-    $("img.scrollToTop").click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, "slow");
-    });
-    $(".thumbnail").colorbox({
-        rel: "thumbnail",
-        transition: "fade",
-        width: "50%",
-        height: "50%"
-    });
-});
-</script>
+    </script>
 
-                                                                            
+
 </body>
+
 </html>
